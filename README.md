678d83579000fd42463445fcf9a5c146d79810ff
Kenji Hirabayashi <mkhira2@gmail.com>
2017-03-27 22:42:15 -0500

    readme

---
+++ README.md
@@ -0,0 +1,1 @@
+introduction to api's p1
--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,21 @@
-console.log('do we hve jquery?')
-console.log($)
-
+// function takes country object as input and returns string containing country names and capitals
 var getCountryData = function(countryObj) {
-	var countryStr = ''
-	countryStr += '<div class="country">' + '<h1 class="countryName">' + countryObj.name + '</h1>'
-	countryStr += '<h2 class="capital">' + countryObj.capital + '</h2>' + '</div>'
-	return countryStr
+	var countryStr = '' // set emptry string
+	countryStr += '<div class="country">' + '<h1 class="countryName">' + countryObj.name + '</h1>' // add country name to empty string in h1 tag
+	countryStr += '<h2 class="capital">' + countryObj.capital + '</h2>' + '</div>' // add country capital to empty string in h2 tag
+	return countryStr // make string available for out-of-function use
 }
 
+// function takes the api response as input and ouputs a string (built by data from the fulfilled promise) to our container div
 var handleResponse = function(apiResponse) {
-	var stringHTML = ''
-	var containerNode = document.querySelector('.container')
-	console.log('now we got the data, right?')
-	for (var i = 0; i < apiResponse.length; i++) {
-		stringHTML += getCountryData(apiResponse[i])
+	var stringHTML = '' // set empty string
+	var containerNode = document.querySelector('.container') // access container div from DOM
+	for (var i = 0; i < apiResponse.length; i++) { // while iterating through the api response given to us by the promise....
+		stringHTML += getCountryData(apiResponse[i]) // ...build our string using the above function
 	}
-	containerNode.innerHTML = stringHTML
+	containerNode.innerHTML = stringHTML // add string to our container div
 }
 
-var promise = $.getJSON('https://restcountries.eu/rest/v1/region/oceania')
+var promise = $.getJSON('https://restcountries.eu/rest/v1/region/oceania') // allows us access to the rest countries api in json format
 
-promise.then(handleResponse)
+promise.then(handleResponse) // returns promise
